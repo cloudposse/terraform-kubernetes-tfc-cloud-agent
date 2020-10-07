b48359d1382812df960396968bc9aea634e39f64
Dan Meyers <danjbh@users.noreply.github.com>
2020-10-06 19:48:22 -0700

    Initial commit

--- main.tf
+++ main.tf
@@ -1,13 +1,44 @@
-resource "random_integer" "example" {
-  count = module.this.enabled ? 1 : 0
-
-  min = 1
-  max = 50000
-  keepers = {
-    example = var.example
+resource "kubernetes_deployment" "tfc-agent" {
+  metadata {
+    name = var.deployment_name
+    namespace = var.k8s_namespace
+    labels = var.labels
   }
-}
+  spec {
+    selector {
+      match_labels = var.selector_match_labels
+    }
+    replicas = var.replicas
 
-locals {
-  example = format("%v %v", var.example, join("", random_integer.example[*].result))
+    template {
+      metadata {
+        labels = var.labels
+        annotations = var.annotations
+      }
+      spec {
+        container {
+          image = var.tfc_agent_image
+          name  = "tfc-agent"
+          env {
+            name  = "TFC_AGENT_TOKEN"
+            value = var.tfc_agent_token
+          }
+          env {
+            name  = "TFC_AGENT_NAME"
+            value = var.tfc_agent_name
+          }
+          resources {
+            limits {
+              cpu    = "1"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,50 @@
-variable "example" {
-  description = "Example variable"
-  default     = "hello world"
+variable "deployment_name" {
+  type    = string
+  default = "tfc-agent"
+}
+
+variable "k8s_namespace" {
+  type        = string
+  default     = "default"
+  description = "Kubernetes namespace to deploy agent within"
+}
+
+variable "labels" {
+  type    = map
+  default = {
+    app   = "tfc-agent"
+  }
+}
+
+variable "selector_match_labels" {
+  type    = map
+  default = {
+    app = "tfc-agent"
+  }
+}
+
+variable "replicas" {
+  type    = number
+  default = 1
+}
+
+variable "annotations" {
+  type    = map
+  default = {
+    "sidecar.istio.io/inject" = "false"
+  }
+}
+
+variable "tfc_agent_image" {
+  type        = string
+  description = "Name and tag of TFC agent docker image"
+  default     = "hashicorp/tfc-agent:latest"
+}
+
+variable "tfc_agent_name" {
+  type = string
+}
+
+variable "tfc_agent_token" {
+  type = string
 }
