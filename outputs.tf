4d3d45cdb6599c6512f7fac8172a78c896155bef
Dan Meyers <danjbh@users.noreply.github.com>
2020-10-13 12:28:17 -0700

    Fixing a few things found during testing

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "service_account_name" {
-  value = local.service_account_name
+  value       = local.service_account_name
+  description = "Name of the Kubernetes service account"
 }
 
 output "namespace" {
-  value = local.namespace
+  value       = local.namespace
+  description = "Name of the Kubernetes namespace"
 }
