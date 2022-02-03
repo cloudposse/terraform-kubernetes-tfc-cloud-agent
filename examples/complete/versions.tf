e57b72cd975edb1fb5d9eb682c101523fc912e52
Adam Buxton <buxton.adam@gmail.com>
2022-02-03 18:31:52 +0000

    update terraform blocks, and version constraints

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,7 +1,17 @@
 terraform {
-  required_version = ">= 0.12.0, < 0.14"
+  // required_version = ">= 0.12.0, < 0.14"
+  required_version = ">= 0.14"
 
   required_providers {
-    kubernetes = "~> 1.12"
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.7.1"
+    }
   }
+}
+
+provider "kubernetes" {
+  # Configuration options
+
+config_path = "~/.kube/config"
 }
--- versions.tf
+++ versions.tf
@@ -1,7 +1,13 @@
 terraform {
-  required_version = ">= 0.12.0, < 0.14.0"
-
+  #  required_version = ">= 0.12.0, < 0.14.0"
+  required_version = ">= 0.14.0"
   required_providers {
-    kubernetes = ">= 1.12.0"
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.7.1"
+    }
   }
 }
+
+
+
